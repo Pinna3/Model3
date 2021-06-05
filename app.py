ea8dd12d926838cafff20fc23876a0b939b74a38
Michael Pinna <mikepinna3@gmail.com>
2021-06-05 07:48:08 -0400

    sqlite database created and populated with contact information

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.Text(30), nullable=False)
+
+    def __repr__(self):
+        return f'{self.id} {self.name}'
 
 
 @app.route('/')
