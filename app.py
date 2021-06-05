068ae938f2b2c11dfb1a83510b16ac31b4c04fc4
Michael Pinna <mikepinna3@gmail.com>
2021-06-05 12:01:16 -0400

    Model3 done

--- app.py
+++ app.py
@@ -20,11 +20,14 @@ def index(id):
     contact = Contact.query.get_or_404(id)
     all_contacts = Contact.query.order_by(Contact.id).all()
     contacts_len = len(Contact.query.all())
+    contacts_index = [i+1 for i in range(contacts_len)]
+    contacts_zipped = zip(all_contacts, contacts_index)
     return render_template(
         'index.html',
         contact=contact,
         contacts=all_contacts,
-        contacts_len=contacts_len
+        contacts_len=contacts_len,
+        contacts_zipped=contacts_zipped
     )
 
 
@@ -38,6 +41,27 @@ def edit(id):
         return redirect(f'/{contact.id}')
     else:
         return render_template('edit.html', contact=contact)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    contact = Contact.query.get_or_404(id)
+    db.session.delete(contact)
+    db.session.commit()
+    return redirect('/1')
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        contact_name = request.form['name']
+        contact_email = request.form['email']
+        new_contact = Contact(name=contact_name, email=contact_email)
+        db.session.add(new_contact)
+        db.session.commit()
+        return redirect(f'/{new_contact.id}')
+    else:
+        return render_template('new.html')
 
 
 if __name__ == '__main__':
